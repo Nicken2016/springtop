44a0d137ff25556704dfb879113e197da45d122b
Nicken <onicken@yandex.ru>
2017-03-14 00:52:03 +0600

    3_08_add_spring_test

---
+++ src/test/java/net/nicken/web/mock/InMemoryAdminRestControllerSpringTest.java
@@ -0,0 +1,53 @@
+package net.nicken.web.mock;
+
+import net.nicken.UserTestData;
+import net.nicken.model.Role;
+import net.nicken.model.User;
+import net.nicken.repository.UserRepository;
+import net.nicken.util.exception.NotFoundException;
+import net.nicken.web.user.AdminRestController;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Collection;
+
+import static net.nicken.UserTestData.ADMIN;
+import static net.nicken.UserTestData.USER;
+
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class InMemoryAdminRestControllerSpringTest {
+
+    @Autowired
+    private AdminRestController controller;
+
+    @Autowired
+    private UserRepository repository;
+
+    @Before
+    public void setUp() throws Exception{
+        repository.getAll().forEach(u -> repository.delete(u.getId()));
+        repository.save(USER);
+        repository.save(ADMIN);
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        controller.delete(UserTestData.USER_ID);
+        Collection<User> users = controller.getAll();
+        Assert.assertEquals(users.size(), 1);
+        Assert.assertEquals(users.iterator().next(), ADMIN);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testDeleteNotFound() throws Exception {
+        controller.delete(10);
+    }
+
+
+}
