4439d626e57c679dcd2e9f64cd2d8f573bedab57
Nicken <onicken@yandex.ru>
2017-05-25 00:23:39 +0600

    6_01_HW5_data_jpa

--- src/main/java/net/nicken/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/net/nicken/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -4,38 +4,49 @@ import net.nicken.model.Meal;
 import net.nicken.repository.MealRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
 
-@Repository
+
 public class DataJpaMealRepositoryImpl implements MealRepository{
 
     @Autowired
-    private CrudMealRepository crudRepository;
+    private CrudMealRepository crudMealRepository;
+
+    @Autowired
+    private CrudUserRepository crudUserRepository;
 
     @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
-        return null;
+        if (!meal.isNew() && get(meal.getId(), userId) == null){
+            return null;
+        }
+
+        meal.setUser(crudUserRepository.getOne(userId));
+        return crudMealRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudMealRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        Meal meal = crudMealRepository.findOne(id);
+        return meal != null && meal.getUser().getId() == userId ? meal : null;
     }
 
     @Override
     public Collection<Meal> getAll(int userId) {
-        return null;
+        return crudMealRepository.getAll(userId);
     }
 
     @Override
-    public Collection<Meal> getBetween(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+    public Collection<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
+        return crudMealRepository.getBetween(startDate, endDate, userId);
     }
 }
