ff126bf8326c899a82c1e51de2ea284f49a6e1c5
Nicken <onicken@yandex.ru>
2017-06-03 14:30:06 +0600

    6_12_jsp_jstl_i18n

--- src/main/java/net/nicken/web/UserServlet.java
+++ src/main/java/net/nicken/web/UserServlet.java
@@ -1,10 +1,14 @@
 package net.nicken.web;
 
 import net.nicken.AuthorizedUser;
+import net.nicken.web.user.AdminRestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +17,15 @@ import java.io.IOException;
 
 public class UserServlet extends HttpServlet{
     private static final Logger LOG = LoggerFactory.getLogger(UserServlet.class);
+
+    private AdminRestController adminController;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        WebApplicationContext springContext = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+        adminController = springContext.getBean(AdminRestController.class);
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -24,6 +37,8 @@ public class UserServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         LOG.debug("forward to users");
+        LOG.debug("getAll");
+        request.setAttribute("users", adminController.getAll());
         request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
 }
---
+++ src/main/webapp/fragments/headTag.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<fmt:setBundle basename="messages.app"/>
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title><fmt:message key="app.title"/> </title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
