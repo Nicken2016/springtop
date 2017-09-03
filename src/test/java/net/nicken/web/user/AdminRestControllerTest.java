c930ac1e69055c35a65449e23dc1bfb9fa4ee6da
Nicken <onicken@yandex.ru>
2017-09-03 22:43:36 +0600

    10_08_not_found_422

--- src/test/java/net/nicken/web/user/AdminRestControllerTest.java
+++ src/test/java/net/nicken/web/user/AdminRestControllerTest.java
@@ -35,8 +35,17 @@ public class AdminRestControllerTest extends AbstractControllerTest{
     }
 
     @Test
+    public void testGetNotFound() throws Exception{
+        mockMvc.perform(get(REST_URL + 1)
+                .with(TestUtil.userHttpBasic(ADMIN)))
+                .andExpect(status().isUnprocessableEntity())
+                .andDo(print());
+    }
+
+
+    @Test
     public void testGetByEmail() throws Exception{
-        mockMvc.perform(get(REST_URL + "by?email=" + USER.getEmail())
+        mockMvc.perform(get(REST_URL + "by?email=" + ADMIN.getEmail())
                 .with(userHttpBasic(ADMIN)))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
@@ -50,6 +59,14 @@ public class AdminRestControllerTest extends AbstractControllerTest{
                 .andDo(print())
                 .andExpect(status().isOk());
         MATCHER.assertCollectionEquals(Collections.singletonList(ADMIN), userService.getAll());
+    }
+
+    @Test
+    public void testDeleteNotFound() throws Exception{
+        mockMvc.perform(delete(REST_URL + 1)
+                .with(TestUtil.userHttpBasic(ADMIN)))
+                .andExpect(status().isUnprocessableEntity())
+                .andDo(print());
     }
 
     @Test
